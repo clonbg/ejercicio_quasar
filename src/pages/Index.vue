d23f022ae3e48ebf830e944672a6c23c9a3622b3
Manuel Riquelme <clonbg@gmail.com>
2021-10-11 19:39:45 +0200

    Inicio

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+    <p>Lorem ipsum dolor sit amet?</p>
+    <q-img
+      :src="'https://placeimg.com/500/300/nature?t=' + Math.random()"
+      style="max-height: 140rem; max-width: 200rem"
+    ></q-img>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
+
+<style scoped></style>
