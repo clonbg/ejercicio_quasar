d762388ffc1e84b3ba05ea668e71d9dc3654c702
Manuel Riquelme <clonbg@gmail.com>
2021-10-24 13:40:27 +0200

    links en el titulo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,46 +1,52 @@
 <template>
   <q-page>
     <div class="flex flex-center q-pt-xl">
-    <div
-      class="row full-width reverse-wrap q-pa-xl q-px-xl"
-      v-for="item in filtroPaginacion"
-      :key="item.id"
-    >
-      <div class="col-sm-7 q-px-sm full-height">
-        <div class="text-h5 q-mt-sm q-mb-xs" v-html="item.title"></div>
-        <div class="text-overline text-blue">{{ item.date }}</div>
-        <div class="text-overline text-red">
-          <span v-for="(tag, index) in item.categorias" :key="index"
-            >#/{{ tag + " " }}
-          </span>
-        </div>
-        <div class="self-center">
-          <div
-            class="text-body1 text-grey-10 justify-between"
-            v-html="item.description"
-          ></div>
-          <div class="row full-width">
-            <q-btn color="secondary" label="Leer más.." :to="{ path: item.id, params: { markdown: item.id } }">
-            </q-btn>
+      <div
+        class="row full-width reverse-wrap q-pa-xl q-px-xl"
+        v-for="item in filtroPaginacion"
+        :key="item.id"
+      >
+        <div class="col-sm-7 q-px-sm full-height">
+          <router-link :to="{ path: item.id, params: { markdown: item.id } }"
+            ><div class="text-h5 q-mt-sm q-mb-xs" v-html="item.title"></div
+          ></router-link>
+          <div class="text-overline text-blue">{{ item.date }}</div>
+          <div class="text-overline text-red">
+            <span v-for="(tag, index) in item.categorias" :key="index"
+              >#/{{ tag + " " }}
+            </span>
+          </div>
+          <div class="self-center">
+            <div
+              class="text-body1 text-grey-10 justify-between"
+              v-html="item.description"
+            ></div>
+            <div class="row full-width">
+              <q-btn
+                color="secondary"
+                label="Leer más.."
+                :to="{ path: item.id, params: { markdown: item.id } }"
+              >
+              </q-btn>
+            </div>
           </div>
         </div>
+        <div class="col-sm-5 col-12 q-px-sm">
+          <img
+            class="rounded-borders"
+            :src="`./../../${item.id}${item.imagen}`"
+            style="width: 100%"
+          />
+        </div>
       </div>
-      <div class="col-sm-5 col-12 q-px-sm">
-        <img
-          class="rounded-borders"
-          :src="`./../../${item.id}${item.imagen}`"
-          style="width: 100%"
+      <div class="flex flex-center q-pa-xl q-mx-auto">
+        <q-pagination
+          v-model="current"
+          :max="Math.floor(entrys.length / 10) + 1"
+          input
+          input-class="text-orange-10"
         />
       </div>
-    </div>
-    <div class="flex flex-center q-pa-xl q-mx-auto">
-      <q-pagination
-      v-model="current"
-      :max="Math.floor(entrys.length/10)+1"
-      input
-      input-class="text-orange-10"
-    />
-  </div>
     </div>
   </q-page>
 </template>
@@ -54,20 +60,27 @@ export default defineComponent({
   setup() {
     return {
       entrys: stories,
-      current: ref(1)
+      current: ref(1),
     };
   },
   computed: {
-    filtroPaginacion (){
+    filtroPaginacion() {
       var entradas;
-      entradas = this.entrys.slice((this.current*10)-10,this.current*10)
-      console.log(this.current,entradas)
-      return entradas
-    }
+      entradas = this.entrys.slice(this.current * 10 - 10, this.current * 10);
+      console.log(this.current, entradas);
+      return entradas;
+    },
   },
   methods: {},
   created() {},
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+a:link,
+a:visited,
+a:active {
+  text-decoration: none;
+  color: black;
+}
+</style>
