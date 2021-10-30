644da4c1e13a1a8bc4f6593d3048aa1bb369d6a5
clonbg <clonbg@gmail.com>
2021-10-30 09:36:00 +0200

    disqus funciona pero siempre son los mismos

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -3,13 +3,10 @@
     <div class="row full-width reverse-wrap q-pa-xl q-px-xl">
       <div class="col q-px-sm full-height">
         <q-markdown :src="post" style="font-size: 120%"></q-markdown>
-        <div id="disqus_thread"></div>
-        <noscript
-          >Please enable JavaScript to view the
-          <a href="https://disqus.com/?ref_noscript"
-            >comments powered by Disqus.</a
-          ></noscript
-        >
+        <section class="comments" aria-labelledby="comment">
+          <h2 id="comment">Comments</h2>
+          <Disqus shortname="clonbg" ref="disqus" :pageConfig="pageConfig" />
+        </section>
         <br />
         <q-btn color="secondary" label="Volver" @click="volver"> </q-btn>
       </div>
@@ -18,18 +15,23 @@
 </template>
 
 <script>
+import { Disqus } from "vue-disqus";
 export default {
+  components: {
+    Disqus,
+  },
   props: ["markdown"],
   data() {
     return {
       post: "",
+      url: "",
+      id: "",
+      pageConfig: {
+        url: window.location.href,
+      },
     };
   },
-  watch: {
-   url(value) {
-      this.$refs.disqus.reset(window.DISQUS)
-   }
-},
+  watch: {},
   methods: {
     scrollToTop() {
       window.scrollTo(0, 0);
@@ -37,37 +39,18 @@ export default {
     volver() {
       this.$router.go(-1);
     },
-    montarDisqus() {
-      /**
-       *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-       *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-       window.DISQUS.reset({
-   reload: true,
-   config: function() {
-      this.page.identifier = window.location.origin + '/#!' + this.$route.path
-   this.page.url = window.location.origin + '/#!' + this.$route.path
-   }
-});
-      console.log(window.location.origin + '/#!' + this.$route.path)
-
-      (function () {
-        // DON'T EDIT BELOW THIS LINE
-        var d = document,
-          s = d.createElement("script");
-        s.src = "https://clonbg.disqus.com/embed.js";
-        s.setAttribute("data-timestamp", +new Date());
-        (d.head || d.body).appendChild(s);
-      })();
-    },
   },
   created() {
     const texto = require(`../markdowns/stories/${this.$route.params.markdown}.md`);
     this.post = texto.default;
-    // console.log(this.$route.params.markdown);
     this.scrollToTop();
-    // console.log(this.$route.path)
-    this.montarDisqus();
+    this.url = window.location.href;
+    this.id = this.$route.path.slice(1, this.$route.path.length);
+    console.log(this.id, this.url);
   },
+  mounted(){
+    this.$refs.disqus.reset
+  }
 };
 </script>
 
