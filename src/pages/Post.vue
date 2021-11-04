fe466cf06ce789ad9004644f76780d0fb835f67a
clonbg <clonbg@gmail.com>
2021-11-04 15:23:13 +0100

    Comentarios de glosa - ver

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -3,7 +3,36 @@
     <div class="row full-width reverse-wrap q-pa-xl q-px-xl">
       <div class="col q-px-sm full-height">
         <q-markdown :src="post" style="font-size: 120%"></q-markdown>
-        <!-- Aquí van los comentarios -->
+        <div v-if="comentarios">
+          <!-- Unas clases condicionales.... -->
+          <q-list
+            bordered
+            class="rounded-borders q-ma-xl"
+            style="max-width: 35rem"
+            v-for="item in comentarios"
+            :key="item.id"
+          >
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar>
+                  <span class="material-icons full-width">
+                    account_circle
+                  </span>
+                </q-avatar>
+              </q-item-section>
+              <!-- Si es hijo de otro... -->
+              <q-item-section>
+                <q-item-label lines="1">{{ item.author }}</q-item-label>
+                <q-item-label caption lines="2">
+                  <span class="text-weight-bold"></span>
+                  {{ item.message }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+        <div v-else>Por que?</div>
+        {{ comentarios }}
         <br />
         <q-btn color="secondary" label="Volver" @click="volver"> </q-btn>
       </div>
@@ -12,11 +41,15 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   props: ["markdown"],
-  setup() {
+  data() {
     return {
       post: "",
+      comentarios: [],
+      text: ref(""),
+      dense: ref(false),
     };
   },
   watch: {},
@@ -27,14 +60,20 @@ export default {
     volver() {
       this.$router.go(-1);
     },
+    async cargarComentarios() {
+      let response = await this.$axios.get(
+        "http://cwhoami.duckdns.org:58155/api/v1/comments/?url=https://glosa.example/best-SO/"
+      );
+      this.comentarios = response.data;
+      console.log(this.comentarios);
+    },
   },
-  created() {
+  async mounted() {
     const texto = require(`../markdowns/stories/${this.$route.params.markdown}.md`);
     this.post = texto.default;
+    await this.cargarComentarios();
     this.scrollToTop();
   },
-  mounted(){
-  }
 };
 </script>
 
