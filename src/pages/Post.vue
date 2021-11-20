fde40b058928c6982e2efdfb25d36238d31de5be
clonbg <clonbg@gmail.com>
2021-11-20 13:21:13 +0100

    twitter a la derecha

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -3,10 +3,8 @@
     <div class="row full-width reverse-wrap q-pa-xl q-px-xl">
       <div class="col q-px-sm full-height">
         <q-markdown :src="post" style="font-size: 120%"></q-markdown>
-        <q-icon size="xl" name="img:icons/twitter.png" class="q-pa-md cursor-pointer" @click="twitter" /><strong>Comentar en twitter</strong>
-        <br />
-        <q-btn color="secondary" label="Volver" @click="volver">
-        </q-btn>
+        <q-btn color="secondary" label="Volver" @click="volver" class="q-my-sm"></q-btn>
+        <q-icon size="xl" name="img:icons/twitter.png" class="cursor-pointer float-right" @click="twitter" />
       </div>
     </div>
   </q-page>
@@ -41,9 +39,9 @@ export default {
     const texto = require(`../markdowns/stories/${this.$route.params.markdown}.md`);
     this.post = texto.default;
     this.scrollToTop();
-    this.web = window.location.href.replace('#','%23')
+    this.web = window.location.href.replace('#', '%23')
     let item = stories.find(element => element.id == this.$route.params.markdown);
-    let itemtags =item.categorias
+    let itemtags = item.categorias
     this.categorias = itemtags.toString()
   }
 };
