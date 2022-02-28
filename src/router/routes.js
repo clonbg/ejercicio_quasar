0c077c27e25377580b7bbb26d1cbe712f7fcff1a
Manuel Riquelme <clonbg@gmail.com>
2022-02-28 10:25:31 +0000

    error 404 no funcionaba

--- src/router/routes.js
+++ src/router/routes.js
@@ -36,16 +36,10 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: ':markdown',component: () => import('pages/Post.vue') }
+      { path: ':markdown',component: () => import('pages/Post.vue') },
+      { path: '*', component: () => import('pages/Error404.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
 ]
 
 export default routes
